6bd354c09e15c38333f07ad32c7d1a297b36f8b8
Elivandro <Elivandro@hotmail.com.br>
2022-10-18 16:02:22 -0400

    routes created

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BeerController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+require __DIR__.'/auth.php';
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,4 +13,11 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+Route::group([
+        'prefix' => 'beers'
+    ], function (){
+
+    Route::get('/', [BeerController::class, 'index']);
+    Route::get('/export', [BeerController::class, 'export']);
+
+});
