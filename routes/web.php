76fd8b3c02a5553e8233b5ec8a9c5ecd5017253e
Elivandro <Elivandro@hotmail.com.br>
2022-10-20 13:55:37 -0400

    inertiaJS, export e report routes

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,34 @@
 <?php
 
+use Inertia\Inertia;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BeerController;
+use App\Http\Controllers\ExportController;
 
 require __DIR__.'/auth.php';
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::group([
-        'prefix' => 'beers'
-    ], function (){
+    'prefix' => 'beers',
+    'middleware' => 'auth'
+], function (){
 
-    Route::get('/', [BeerController::class, 'index']);
-    Route::get('/export', [BeerController::class, 'export']);
+Route::get('/', [BeerController::class, 'index']);
+Route::get('/export', [BeerController::class, 'export']);
 
+Route::resource('/reports', ExportController::class)
+    ->only(["index", "destroy"]);
 });
